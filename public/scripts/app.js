a4015a33b40ea71b88f1dae2d205da4e4e1a2d07
Shalini-Venugopal <shalini.venogopal@nutechnologyinc.com>
2020-12-22 17:56:03 +0530

    Intro of JSX(JavaScript XML)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,42 @@ console.log("App.js is running");
 
 //JSX code
 var template = React.createElement(
-  "h1",
-  null,
-  "It will be changed!!!"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Hello World!!!"
+    ),
+    React.createElement(
+        "h1",
+        null,
+        "Name : Shalini"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "It contains some Info"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Place : Komarapalayam"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Degree : B.E."
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Stream : CSE"
+        )
+    )
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 console.log("App.js is running");
 
 //JSX code
-var template = <h1>It will be changed!!!</h1>;
+var template = (
+    <div>
+        <h1>Hello World!!!</h1>
+        <h1>Name : Shalini</h1>
+        <p>It contains some Info</p>
+        <p>Place : Komarapalayam</p>
+        <ol>
+            <li>Degree : B.E.</li>
+            <li>Stream : CSE</li>
+        </ol>
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
