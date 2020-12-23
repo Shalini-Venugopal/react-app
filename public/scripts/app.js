eb559e07affb659fa062359ce7f208b1c0b7925d
Shalini-Venugopal <shalini.venogopal@nutechnologyinc.com>
2020-12-23 18:50:38 +0530

    Events and Attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,80 @@
 'use strict';
 
-var add = function add(a, b) {
-    return a + b;
+console.log("App.js is running");
+
+// const app ={
+//     title : 'React App',
+//     subtitle : 'Put your life on Learning',
+//     options : ['React JS', 'Java']
+// };
+// const templateTwo = (
+//     <div>
+//         <h1>{app.title}</h1>
+//         {app.subtitle && <p>{app.subtitle}</p>}
+//         <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+//         <ol>
+//             <li>List 1</li>
+//             <li>List 2</li>
+//         </ol>
+//     </div>
+
+// );
+
+
+// const user ={
+//     name : 'ABCD',
+//     age : 25,
+//     location : 'USA'
+// };
+// function getLocation(location){
+//     if (location){
+//         return <p>Location : {location}</p>;
+//     }
+// }
+// const template = (
+//     <div>
+//         <h1>Name : {user.name ? user.name : 'Anonymous'}</h1>
+//         {user.age >=18 && <p>Age :{user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// );
+
+var count = 0;
+var addOne = function addOne() {
+    console.log('addOne');
 };
-console.log(add(50, 1));
-
-var user = {
-    name: 'Shalini',
-    cities: ['Komarapalayam', 'Chennai'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-    }
+var minusOne = function minusOne() {
+    console.log('minusOne');
 };
-console.log(user.printPlacesLived());
-
-var multiplier = {
-    numbers: [5, 10, 15],
-    multiplyBy: 5,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
+var reset = function reset() {
+    console.log('reset');
 };
-console.log(multiplier.multiply());
+var counter = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count : ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        'Count+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        'Count-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'Reset'
+    )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(counter, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,42 +1,61 @@
 console.log("App.js is running");
 
-const app ={
-    title : 'React App',
-    subtitle : 'Put your life on Learning',
-    options : ['React JS', 'Java']
+// const app ={
+//     title : 'React App',
+//     subtitle : 'Put your life on Learning',
+//     options : ['React JS', 'Java']
+// };
+// const templateTwo = (
+//     <div>
+//         <h1>{app.title}</h1>
+//         {app.subtitle && <p>{app.subtitle}</p>}
+//         <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+//         <ol>
+//             <li>List 1</li>
+//             <li>List 2</li>
+//         </ol>
+//     </div>
+
+// );
+
+
+// const user ={
+//     name : 'ABCD',
+//     age : 25,
+//     location : 'USA'
+// };
+// function getLocation(location){
+//     if (location){
+//         return <p>Location : {location}</p>;
+//     }
+// }
+// const template = (
+//     <div>
+//         <h1>Name : {user.name ? user.name : 'Anonymous'}</h1>
+//         {user.age >=18 && <p>Age :{user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// );
+
+let count = 0;
+const addOne = () => {
+    console.log('addOne');
 };
-const templateTwo = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>List 1</li>
-            <li>List 2</li>
-        </ol>
-    </div>
-
-);
-
-
-const user ={
-    name : 'ABCD',
-    age : 25,
-    location : 'USA'
+const minusOne = () => {
+    console.log('minusOne');
 };
-function getLocation(location){
-    if (location){
-        return <p>Location : {location}</p>;
-    }
-}
-const template = (
+const reset = () => {
+    console.log('reset');
+};
+const counter = (
     <div>
-        <h1>Name : {user.name ? user.name : 'Anonymous'}</h1>
-        {user.age >=18 && <p>Age :{user.age}</p>}
-        {getLocation(user.location)}
+        <h1>Count : {count}</h1>
+        <button onClick={addOne}>Count+1</button>
+        <button onClick= {minusOne}>Count-1</button>
+        <button onClick={reset}>Reset</button>
     </div>
-);
+)
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(counter, appRoot);
