cfd1c3d6bbcb2a8d7a13d31d7e070f558ee44642
Shalini-Venugopal <shalini.venogopal@nutechnologyinc.com>
2020-12-23 12:23:57 +0530

    Conditional Rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,39 +2,66 @@
 
 console.log("App.js is running");
 
-var user = {
-    name: 'ABCD',
-    title: 'Ponniyin Selvan',
-    author: 'Kalki Krishnamurthy'
+var app = {
+    title: 'React App',
+    subtitle: 'Put your life on Learning',
+    options: ['React JS', 'Java']
 };
-var template = React.createElement(
+var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        'Hello World'
+        app.title
     ),
-    React.createElement(
-        'h1',
+    app.subtitle && React.createElement(
+        'p',
         null,
-        'Name : ',
-        user.name + '!'
+        app.subtitle
     ),
     React.createElement(
         'p',
         null,
-        'Age :',
-        user.title
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
-        'p',
+        'ol',
         null,
-        'Location: ',
-        user.author
+        React.createElement(
+            'li',
+            null,
+            'List 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'List 2'
+        )
     )
 );
 
+// var user ={
+//     name : 'ABCD',
+//     age : 25,
+//     location : 'USA'
+// };
+// function getLocation(location){
+//     if (location){
+//         return <p>Location : {location}</p>;
+//     }
+//     // else {
+//     //     return 'Invalid';
+//     // }
+// }
+// var template = (
+//     <div>
+//         <h1>Name : {user.name ? user.name : 'Anonymous'}</h1>
+//         {user.age >=18 && <p>Age :{user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// );
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,19 +1,45 @@
 console.log("App.js is running");
 
-var user ={
-    name : 'ABCD',
-    title : 'Ponniyin Selvan',
-    author : 'Kalki Krishnamurthy'
+var app ={
+    title : 'React App',
+    subtitle : 'Put your life on Learning',
+    options : ['React JS', 'Java']
 };
-var template = (
+var templateTwo = (
     <div>
-        <h1>Hello World</h1>
-        <h1>Name : {user.name + '!'}</h1>
-        <p>Age :{user.title}</p>
-        <p>Location: {user.author}</p>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>List 1</li>
+            <li>List 2</li>
+        </ol>
     </div>
+
 );
+
+
+// var user ={
+//     name : 'ABCD',
+//     age : 25,
+//     location : 'USA'
+// };
+// function getLocation(location){
+//     if (location){
+//         return <p>Location : {location}</p>;
+//     }
+//     // else {
+//     //     return 'Invalid';
+//     // }
+// }
+// var template = (
+//     <div>
+//         <h1>Name : {user.name ? user.name : 'Anonymous'}</h1>
+//         {user.age >=18 && <p>Age :{user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
