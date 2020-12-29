dc4f942f4e8ca8632cdd3c8a9df76db40d7c20f5
Shalini-Venugopal <shalini.venogopal@nutechnologyinc.com>
2020-12-29 17:37:05 +0530

    Component

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,58 +1,34 @@
 class VisibilityToggle extends React.Component {
     constructor(props) {
-        super(props);
-        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
-        this.state = {
-            visibility:false
-        };
+      super(props);
+      this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+      this.state = {
+        visibility: false
+      };
     }
-    handleToggleVisibility(){
-        this.setState((prevState) => {
-            return {
-                visibility: !prevState.visibility
-            };
-        });
+    handleToggleVisibility() {
+      this.setState((prevState) => {
+        return {
+          visibility: !prevState.visibility
+        };
+      });
     }
     render() {
-        return (
+      return (
+        <div>
+          <h1>Visibility Toggle</h1>
+          <button onClick={this.handleToggleVisibility}>
+            {this.state.visibility ? 'Hide details' : 'Show details'}
+          </button>
+          {this.state.visibility && (
             <div>
-                <h1>Visibility Toggle</h1>
-                <button onClick = {this.handleToggleVisibility}>
-                    {this.state.visibility ? 'Hide Details' : 'Show Details'}
-                </button>
-                {this.state.visibility && (
-                    <div>
-                        <p>Hey, I am there</p>
-                    </div>
-                )}
+              <p>Hey. These are some details you can now see!</p>
             </div>
-        );
+          )}
+        </div>
+      );
     }
-}
-ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-// let visibility = 'false';
-
-// const toggleVisibility = () => {
-//     visibility = !visibility;
-//     render();
-// };
-
-// const render = () => {
-    // const jsx = (
-    //     <div>
-    //         <h1>Visibility Toggle</h1>
-    //         <button onClick = {toggleVisibility}>
-    //             {!visibility ? 'Hide Details' : 'Show Details'}
-    //         </button>
-    //         {!visibility && (
-    //             <div>
-    //                 <p>Hey, I am there</p>
-    //             </div>
-    //         )}
-    //     </div>
-    // );
-//     ReactDOM.render(jsx, document.getElementById('app'));
-// };
-
-// render();
+  }
+  
+  ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
+  
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,94 +1,58 @@
 class Counter extends React.Component {
-
     constructor(props) {
-        super(props);
-        this.handleAddOne = this.handleAddOne.bind(this);
-        this.handleMinusOne = this.handleMinusOne.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-        this.state = {
-            count : 0
-        };
+      super(props);
+      this.handleAddOne = this.handleAddOne.bind(this);
+      this.handleMinusOne = this.handleMinusOne.bind(this);
+      this.handleReset = this.handleReset.bind(this);
+      this.state = {
+        count: 0
+      };
     }
-
     componentDidMount() {
-        const stringCount = localStorage.getItem('count');
-        const count = parseInt(stringCount,10);     //base 10
-
-        if(!isNaN(count)) {
-            this.setState(() => ({count}))
-        }
+      const stringCount = localStorage.getItem('count');
+      const count = parseInt(stringCount, 10);
+  
+      if (!isNaN(count)) {
+        this.setState(() => ({ count }));
+      }
     }
     componentDidUpdate(prevProps, prevState) {
-        if (prevState.count !== this.state.count) {
-            localStorage.setItem('count', this.state.count);
-        }
+      if (prevState.count !== this.state.count) {
+        localStorage.setItem('count', this.state.count);
+      }
     }
-
-    handleAddOne(){
-        //console.log('handleAddOne');
-        this.setState((prevState) =>{
-            return {
-                count : prevState.count + 1
-            };
-        });
+    handleAddOne() {
+      this.setState((prevState) => {
+        return {
+          count: prevState.count + 1
+        };
+      });
     }
-    handleMinusOne(){
-        //console.log('handleMinusOne');
-        this.setState((prevState) =>{
-            return {
-                count : prevState.count - 1
-            };
-        });
+    handleMinusOne() {
+      this.setState((prevState) => {
+        return {
+          count: prevState.count - 1
+        };
+      });
     }
-    handleReset(){
-        //console.log('handleReset');
-        this.setState(() => {
-            return {
-                count : 0
-            }
-        });
+    handleReset() {
+      this.setState(() => {
+        return {
+          count: 0
+        };
+      });
     }
     render() {
-        return (
-            <div>
-                <h1>Count : {this.state.count}</h1>
-                <button onClick={this.handleAddOne}>Count+1</button>
-                <button onClick={this.handleMinusOne}>Count-1</button>
-                <button onClick={this.handleReset}>Reset</button>
-            </div>
-        );
+      return (
+        <div>
+          <h1>Count: {this.state.count}</h1>
+          <button onClick={this.handleAddOne}>+1</button>
+          <button onClick={this.handleMinusOne}>-1</button>
+          <button onClick={this.handleReset}>reset</button>
+        </div>
+      );
     }
-}
-
-ReactDOM.render(<Counter />, document.getElementById('app'));
-
-// let count = 0;
-// const addOne = () => {
-//     //console.log('addOne');
-//     count++;
-//     renderCounterApp();
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// };
-// const appRoot = document.getElementById('app');
-// const renderCounterApp = () => {
-//     const counter = (
-//         <div>
-//             <h1>Count : {count}</h1>
-//             <button onClick={addOne}>Count+1</button>
-//             <button onClick= {minusOne}>Count-1</button>
-//             <button onClick={reset}>Reset</button>
-//         </div>
-//     );
-
-
-
-// ReactDOM.render(counter, appRoot);
-// };
-// renderCounterApp();
+  }
+  
+  ReactDOM.render(<Counter />, document.getElementById('app'));
+  
